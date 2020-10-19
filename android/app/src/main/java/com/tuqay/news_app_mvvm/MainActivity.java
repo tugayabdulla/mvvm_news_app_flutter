7ab0e3f4d93a18f434d4d84dbd12302b05526365
Tuqay Abdullazade <57816441+tuqayabdulla@users.noreply.github.com>
2020-10-20 01:08:32 +0400

    Initial commit

---
+++ android/app/src/main/java/com/tuqay/news_app_mvvm/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tuqay.news_app_mvvm;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/services/news_service.dart
@@ -0,0 +1,5 @@
+const API_KEY = "8a21d3f212ee43f8a400d89642ad37f1";
+
+class NewsService{
+  
+}
